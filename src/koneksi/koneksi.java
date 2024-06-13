7e4ce1209787fc785ea1ffcb72b8ba1d80053790
NarendraKurnia <118795394+NarendraKurnia@users.noreply.github.com>
2024-06-13 10:43:51 +0700

    Add files via upload

---
+++ src/koneksi/koneksi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package koneksi;
+        
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class koneksi {
+    private static final String URL = "jdbc:mysql://localhost:3306/identitas_pasien";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
